8487bf75bdbf3ef447ccb148d77af23ad5810ef7
Alex <alivsaz@gmail.com>
2024-11-22 10:52:26 +0300

    Поправил задержку sleep

--- module_13_1.py
+++ module_13_1.py
@@ -5,7 +5,7 @@ import asyncio
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for i in range(5):
-        await asyncio.sleep(power)
+        await asyncio.sleep(1 / power)
         print(f'Силач {name} поднял {i+1} шар.')
     print(f'Силач {name} закончил соревнования.')
 
